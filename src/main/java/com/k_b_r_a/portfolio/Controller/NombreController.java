b45321118d8fe68caf39a06ab5e3819337d1620c
k-b-r-a <user.email>
2023-01-28 14:45:14 -0300

    first commit

--- src/main/java/com/k_b_r_a/portfolio/Controller/NombreController.java
+++ src/main/java/com/k_b_r_a/portfolio/Controller/NombreController.java
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 /**
  *
  * @author kbra
  */
-@RestController
+@Controller
 @RequestMapping(path="/api")
 
 public class NombreController {
